37ac8ab2cd9ae72a5068475ad7fb345458c3ff0b
Matthew Utley <matthew.utley@availity.com>
2017-06-02 17:10:15 -0500

    Basic new monitor functionality

---
+++ src/app/app.globals.ts
@@ -0,0 +1,9 @@
+export class WebRequestMethod {
+    static Get = 'GET';
+    static Post = 'POST';
+}
+
+export class MonitorType {
+    static WebRequest = 'WebRequest';
+    static Ping = 'Ping';
+}
